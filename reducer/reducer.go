51155a71fa7ad3458aaeadd298fb069d83618040
chungha <chungha@gmail.com>
2014-11-08 15:38:46 +0900

    Reduce and Query 1st version

--- reducer/reducer.go
+++ reducer/reducer.go
@@ -1,16 +1,45 @@
 package reducer
 
 import (
-  "fmt"
+  "strings"
   "container/list"
 )
 
-
+var indexMap = make(map[string]map[string]*list.List)
 
 func Reduce(l *list.List) {
   for e := l.Front(); e != nil; e = e.Next() {
     if v, ok := e.Value.(string); ok {
-      fmt.Printf(v)
+      data := strings.Split(v, " ")
+      urlMap, ok := indexMap[data[0]]
+      if !ok {
+        urlMap = make(map[string]*list.List)
+      }
+      posList, ok := urlMap[data[1]]
+      if !ok {
+        posList = list.New()
+      }
+      posList.PushBack(data[2])
+      urlMap[data[1]] = posList
+      indexMap[data[0]] = urlMap
     }
   }
+}
+
+func Query(w string) string {
+  urlMap, ok := indexMap[w]
+  if !ok {
+    return "none"
+  }
+  var result = w + "\n"
+  for k, v := range urlMap {
+    result += " - " + k + " ["
+    for e := v.Front(); e != nil; e = e.Next() {
+      if p, ok := e.Value.(string); ok {
+       result += p + " "
+      }
+    }
+    result += "]\n"
+  }
+  return result
 }
--- reducer/reducer_test.go
+++ reducer/reducer_test.go
@@ -2,12 +2,15 @@ package reducer
 
 import (
   "testing"
+  "fmt"
   "container/list"
 )
 
 func TestInput(*testing.T) {
   l := list.New()
-  l.PushBack("aaa");
-  l.PushBack("bbb");
+  l.PushBack("word1 url_1 3");
+  l.PushBack("word2 url_2 5");
   Reduce(l)
+
+  fmt.Printf(Query("word1"))
 }
