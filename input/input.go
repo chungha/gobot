965b315ab3b97b1fc363731064fdf444ee1c0b50
nomos <nomos@u1.(none)>
2014-11-29 16:38:58 +0900

    reading from file

--- input/input.go
+++ input/input.go
@@ -4,19 +4,55 @@ package input
 import (
   "fmt"
   "container/list"
+  "bufio"
+  "os"
 )
 
 
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+  file, err := os.Open(path)
+  if err != nil {
+    return nil, err
+  }
+  defer file.Close()
+
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines, scanner.Err()
+}
+
+
 func Input() *list.List {
+
   inputs := list.New();
 
+  lines, err := readLines("urls")
+  if err != nil {
+    fmt.Println("can not read file")
+    return inputs
+  }
+
+  for _, line := range lines {
+    // fmt.Println(i, line)
+    inputs.PushBack(line)
+  }
+
+/* static
   inputs.PushBack("http://en.wikipedia.org/wiki/Go_(programming_language)");
   inputs.PushBack("http://en.wikipedia.org/wiki/Peace");
   inputs.PushBack("http://en.wikipedia.org/wiki/Apple_Inc.");
+*/
 
+/* for testing
   for e := inputs.Front(); e != nil; e = e.Next() {
        fmt.Println(e.Value)
   }
+*/
 
   return inputs
 }
--- input/input_test.go
+++ input/input_test.go
@@ -11,7 +11,8 @@ func Test_Input(*testing.T) {
   inputs := Input();
 
   for e := inputs.Front(); e != nil; e = e.Next() {
-       fmt.Println(e.Value)
+    fmt.Println(e.Value)
   }
+  fmt.Println("total=", inputs.Len())
 }
 
