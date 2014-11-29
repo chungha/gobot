51f2c3fa123addc0db97d9769a7cb6a15efedd41
chungha <chungha@gmail.com>
2014-11-29 14:41:31 +0900

    add gobot.go

---
+++ gobot.go
@@ -0,0 +1,15 @@
+/**
+ * The main function of the gobot.
+ * Input : A text file containing urls.
+ * Output : The time of indexing.
+ */
+package main
+
+import (
+  "fmt"
+)
+
+
+func main() {
+  fmt.Println("Hello! gobot!")
+}
